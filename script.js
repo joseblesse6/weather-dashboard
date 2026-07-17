e7307c2d3ca1c7b26ac8fb447c829669ae8fbb71
Joselin <joseblesse6@gmail.com>
2026-07-17 13:38:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", () => {
     const city = document.getElementById("city").value;
-    document.getElementById("weather").innerHTML = `
+    document.getElementById("weatherResult").innerHTML = `
         <h2>${city}</h2>
         <p>🌡 Temperature : 30°C</p>
         <p>☁ Weather : Clear Sky</p>
