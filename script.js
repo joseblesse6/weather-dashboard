82a92270e8270eb394bdf0b0e754ce2d947b58b3
Joselin <joseblesse6@gmail.com>
2026-07-17 13:32:50 +0530

    Add JavaScript weather preview

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+const searchBtn = document.getElementById("searchBtn");
+searchBtn.addEventListener("click", () => {
+    const city = document.getElementById("city").value;
+    document.getElementById("weather").innerHTML = `
+        <h2>${city}</h2>
+        <p>🌡 Temperature : 30°C</p>
+        <p>☁ Weather : Clear Sky</p>
+        <p>💧 Humidity : 60%</p>
+        <p>🌬 Wind Speed : 10 km/h</p>
+    `;
+});
