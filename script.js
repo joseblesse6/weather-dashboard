ccd698a0998eb68e11bc559d23ba48144d931339
Joselin <joseblesse6@gmail.com>
2026-07-17 13:50:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+const apiKey = "3e99c7c2d2a716096861635415381deb";
 const searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", () => {
     const city = document.getElementById("city").value;
-    document.getElementById("weatherResult").innerHTML = `
-        <h2>${city}</h2>
-        <p>🌡 Temperature : 30°C</p>
-        <p>☁ Weather : Clear Sky</p>
-        <p>💧 Humidity : 60%</p>
-        <p>🌬 Wind Speed : 10 km/h</p>
-    `;
+    if(city===""){
+        alert("Please enter a city name");
+        return;
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        if(data.cod != 200){
+            document.getElementById("weatherResult").innerHTML =
+            "<h2>City not found</h2>";
+            return;
+        }
+        document.getElementById("weatherResult").innerHTML = `
+            <h2>${data.name}</h2>
+            <p>🌡 Temperature : ${data.main.temp} °C</p>
+            <p>☁ Weather : ${data.weather[0].main}</p>
+            <p>💧 Humidity : ${data.main.humidity}%</p>
+            <p>🌬 Wind Speed : ${data.wind.speed} m/s</p>
+        `;
+    });
+
 });
